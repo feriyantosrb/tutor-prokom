d262777c63cecd7490b81ef229bda09642c78c64
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2021-12-16 13:59:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from fileHandler import csvHandler,jsonHandler
 import numpy as np
 import pandas as pd
-import sklearn
+import sklearn as sk
 from sklearn.linear_model import LinearRegression
 import plotly.graph_objects as go
 import plotly.express as px
